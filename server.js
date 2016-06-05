50abfc0e2ef9e24022a7faec829dafb932754345
Robin Ridderholt <robin.ridderhol@live.com>
2016-06-05 18:15:39 +0200

    Adds endpoint for fetching todos from server

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ var express = require('express');
 var fallback = require('express-history-api-fallback');
 var app = express();
 var port = 3000;
+var todos = [
+	{ 
+		id: 1,
+		name: 'Create react app',
+		complete: true 
+	},
+	{
+		id: 2,
+		name: 'Write blog post',
+		complete: false
+	}
+	];
 
 var root = __dirname + '/public';
+app.use('/api/todos', (req, res) => {
+	res.json(todos);
+});
 app.use(express.static(root));
 app.use(express.static(root + '/*'));
 app.use(fallback('index.html', { root: root }));
